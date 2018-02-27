8f1d14d8a9e14ea38b788b2d3de69747b594ab13
Nicolas Rouviere <nicolas.rouviere@amadeus.com>
2018-02-27 16:50:24 +0100

    Make script more user friendly

--- ArmelQEMU/armel_debian.sh
+++ ArmelQEMU/armel_debian.sh
@@ -4,45 +4,67 @@ usage () {
 	cat << EOF >&1
 
 usage: $0 [-l]
-	-l    Launch VM
+    -l    Launch VM
+    -i    Install VM (Basicallly, just download qcow2, initramfs and kernel from 
+          https://people.debian.org/~aurel32/qemu/armel/
+    -h    display this text and exit
 EOF
 }
 
-ARMEL_QCOW2_URL="https://people.debian.org/~aurel32/qemu/armel/debian_wheezy_armel_standard.qcow2"
-ARMEL_QCOW2="${ARMEL_QCOW2_URL##*/}"
+install () {
+	ARMEL_QCOW2_URL="https://people.debian.org/~aurel32/qemu/armel/debian_wheezy_armel_standard.qcow2"
+	ARMEL_QCOW2="${ARMEL_QCOW2_URL##*/}"
 
-ARMEL_KERNEL_URL="https://people.debian.org/~aurel32/qemu/armel/vmlinuz-3.2.0-4-versatile"
-ARMEL_KERNEL="${ARMEL_KERNEL_URL##*/}"
+	ARMEL_KERNEL_URL="https://people.debian.org/~aurel32/qemu/armel/vmlinuz-3.2.0-4-versatile"
+	ARMEL_KERNEL="${ARMEL_KERNEL_URL##*/}"
 
-ARMEL_INITRAMFS_URL="https://people.debian.org/~aurel32/qemu/armel/initrd.img-3.2.0-4-versatile"
-ARMEL_INITRAMFS="${ARMEL_INITRAMFS_URL##*/}"
+	ARMEL_INITRAMFS_URL="https://people.debian.org/~aurel32/qemu/armel/initrd.img-3.2.0-4-versatile"
+	ARMEL_INITRAMFS="${ARMEL_INITRAMFS_URL##*/}"
 
-ARMEL_QCOW2_DEFAULT="debian_DEFAULT.qcow2"
-ARMEL_KERNEL_DEFAULT="vmlinuz_DEFAULT"
-ARMEL_INITRAMFS_DEFAULT="initrd.img-DEFAULT"
+	ARMEL_QCOW2_DEFAULT="debian_DEFAULT.qcow2"
+	ARMEL_KERNEL_DEFAULT="vmlinuz_DEFAULT"
+	ARMEL_INITRAMFS_DEFAULT="initrd.img-DEFAULT"
 
-if [[ ! -f "${ARMEL_QCOW2_DEFAULT}" ]]; then
-	echo "[+++] ${ARMEL_QCOW2} not found. Downloading"
-	curl -o "${ARMEL_QCOW2_DEFAULT}" "${ARMEL_QCOW2_URL}"
-fi
+	if [[ ! -f "${ARMEL_QCOW2_DEFAULT}" ]]; then
+		echo "[+++] ${ARMEL_QCOW2} not found. Downloading"
+		curl -o "${ARMEL_QCOW2_DEFAULT}" "${ARMEL_QCOW2_URL}"
+	fi
 
-if [[ ! -f "${ARMEL_KERNEL_DEFAULT}" ]]; then
-	echo "[+++] ${ARMEL_KERNEL} not found. Downloading"
-	curl -o "${ARMEL_KERNEL_DEFAULT}" "${ARMEL_KERNEL_URL}" 
-fi
+	if [[ ! -f "${ARMEL_KERNEL_DEFAULT}" ]]; then
+		echo "[+++] ${ARMEL_KERNEL} not found. Downloading"
+		curl -o "${ARMEL_KERNEL_DEFAULT}" "${ARMEL_KERNEL_URL}" 
+	fi
 
-if [[ ! -f "${ARMEL_INITRAMFS_DEFAULT}" ]]; then
-	echo "[+++] ${ARMEL_INITRAMFS} not found. Downloading"
-	curl -o "${ARMEL_INITRAMFS_DEFAULT}" "${ARMEL_INITRAMFS_URL}"
-fi
+	if [[ ! -f "${ARMEL_INITRAMFS_DEFAULT}" ]]; then
+		echo "[+++] ${ARMEL_INITRAMFS} not found. Downloading"
+		curl -o "${ARMEL_INITRAMFS_DEFAULT}" "${ARMEL_INITRAMFS_URL}"
+	fi
+}
 
-if [[ $1 == "-l" ]]; then
+launch () {
 	qemu-system-arm -M versatilepb \
 		-kernel vmlinuz-DEFAULT \
 		-initrd initrd.img-DEFAULT \
 		-hda debian_DEFAULT.qcow2 \
 		-append "root=/dev/sda1"
-else
-	usage
-	exit 1
-fi
+}
+
+case "$1" in
+	"-l")
+		launch
+		shift 1
+		;;
+	"-i")
+		install
+		shift 1
+		;;
+	"-h")
+		usage
+		exit 0
+		;;
+	"*")
+		usage
+		exit 1
+		;;
+esac
+
